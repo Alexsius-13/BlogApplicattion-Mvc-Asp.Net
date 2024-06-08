cd23759aa970c8faf2f380750b26599a67ebb350
Aleko Kotashvili <alexkotashvili@gmail.com>
2024-06-08 15:09:38 +0400

    Initial commit

---
+++ Bloggie/Bloggie.Web/Models/Domain/BlogPost.cs
@@ -0,0 +1,17 @@
+﻿namespace Bloggie.Web.Models.Domain
+{
+    public class BlogPost
+    {
+        public Guid Id { get; set; }
+        public string Heading { get; set; }
+        public string PageTitle { get; set; }
+        public string Content { get; set; }
+        public string ShortDescription { get; set; }
+        public string FeaturedImgUrl { get; set; }
+        public string UrlHandler { get; set; }
+        public DateTime PublishDate { get; set; }
+        public string Author { get; set; }
+        public bool Visible { get; set; }
+        public ICollection<Tag> Tags { get; set; }
+    }
+}
