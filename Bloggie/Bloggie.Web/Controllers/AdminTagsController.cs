231339d4a6e9e1b093ad9e6a2969eeec1dc58834
Aleko Kotashvili <alexkotashvili@gmail.com>
2024-06-10 22:59:49 +0400

    Implemented admin functionality for tags addition as well as reading all of them from database. Drawn views for each method

--- Bloggie/Bloggie.Web/Controllers/AdminTagsController.cs
+++ Bloggie/Bloggie.Web/Controllers/AdminTagsController.cs
@@ -1,13 +1,47 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Bloggie.Web.Data;
+using Bloggie.Web.Models.Domain;
+using Bloggie.Web.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Bloggie.Web.Controllers
 {
     public class AdminTagsController : Controller
     {
+        private readonly BloggieDbContext bloggieDbContext;
+
+        public AdminTagsController(BloggieDbContext bloggieDbContext)
+        {
+            this.bloggieDbContext = bloggieDbContext;
+        }
         [HttpGet]
         public IActionResult Add()
         {
             return View();
+        }
+
+        [HttpPost]
+        [ActionName("Add")]
+        public IActionResult Add(AddTagRequest addTagRequest)
+        {
+            var tag = new Tag
+            {
+                Name = addTagRequest.Name,
+                DisplayName = addTagRequest.DisplayName,
+            };
+
+            bloggieDbContext.Tags.Add(tag);
+            bloggieDbContext.SaveChanges();
+
+            return RedirectToAction("List");
+        }
+
+        [HttpGet]
+        [ActionName("List")]
+        public IActionResult List()
+        {
+            var tags = bloggieDbContext.Tags.ToList();
+
+            return View(tags);
         }
     }
 }
