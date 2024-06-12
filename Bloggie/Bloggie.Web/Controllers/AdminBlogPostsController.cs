0b9ac734526a7870275fd4c62d25c5deb5bef83e
Aleko Kotashvili <alexkotashvili@gmail.com>
2024-06-12 12:20:18 +0400

    Drawn View for BlogPost additions according to it's tags. Created ViewModel for displaying purposes of BlogPost's relevant Tags dropdown.

---
+++ Bloggie/Bloggie.Web/Controllers/AdminBlogPostsController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Bloggie.Web.Controllers
+{
+    public class AdminBlogPostsController : Controller
+    {
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+    }
+}
---
+++ Bloggie/Bloggie.Web/Models/ViewModels/AddBlogPostRequest.cs
@@ -0,0 +1,17 @@
+﻿using Bloggie.Web.Models.Domain;
+
+namespace Bloggie.Web.Models.ViewModels
+{
+    public class AddBlogPostRequest
+    {
+        public string Heading { get; set; }
+        public string PageTitle { get; set; }
+        public string Content { get; set; }
+        public string ShortDescription { get; set; }
+        public string FeaturedImgUrl { get; set; }
+        public string UrlHandler { get; set; }
+        public DateTime PublishDate { get; set; }
+        public string Author { get; set; }
+        public bool Visible { get; set; }
+    }
+}
