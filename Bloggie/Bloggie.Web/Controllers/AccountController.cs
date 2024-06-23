d294f2b21022060ee6f84364ee08e473c9102b86
Aleko Kotashvili <alexkotashvili@gmail.com>
2024-06-24 00:28:26 +0400

    Conducted Login and Logout functionality. Authentication is completely done. Now I'll head over to authorization to give users their own rights according to the website's roles in next commit

--- Bloggie/Bloggie.Web/Controllers/AccountController.cs
+++ Bloggie/Bloggie.Web/Controllers/AccountController.cs
@@ -7,10 +7,12 @@ namespace Bloggie.Web.Controllers
 	public class AccountController : Controller
 	{
 		private readonly UserManager<IdentityUser> userManager;
+		private readonly SignInManager<IdentityUser> signInManager;
 
-		public AccountController(UserManager<IdentityUser> userManager)
+		public AccountController(UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signInManager)
         {
 			this.userManager = userManager;
+			this.signInManager = signInManager;
 		}
 
         [HttpGet]
@@ -41,6 +43,33 @@ namespace Bloggie.Web.Controllers
 			}
 
 			return View();
+		}
+
+		[HttpGet]
+		public IActionResult Login()
+		{
+			return View();
+		}
+
+		[HttpPost]
+		public async Task<IActionResult> Login(LoginViewModel loginViewModel)
+		{
+			var signInResult = await signInManager.PasswordSignInAsync(loginViewModel.Username,
+				loginViewModel.Password, false, false);
+
+			if(signInResult != null && signInResult.Succeeded)
+			{
+				return RedirectToAction("Index", "Home");
+			}
+
+			return View();
+		}
+
+		[HttpGet]
+		public async Task<IActionResult> Logout()
+		{
+			await signInManager.SignOutAsync();
+			return RedirectToAction("Index", "Home");
 		}
 	}
 }
