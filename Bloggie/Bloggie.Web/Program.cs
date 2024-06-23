a63858016bf1d67addd7b0f8fc7c847c91367cb6
Aleko Kotashvili <alexkotashvili@gmail.com>
2024-06-23 12:45:26 +0400

    Implemented Register Functionality. Made changes to IdentityOptions regarding password requirements.

--- Bloggie/Bloggie.Web/Program.cs
+++ Bloggie/Bloggie.Web/Program.cs
@@ -17,6 +17,16 @@ options.UseSqlServer(builder.Configuration.GetConnectionString("BloggieAuthDbCon
 builder.Services.AddIdentity<IdentityUser, IdentityRole>()
     .AddEntityFrameworkStores<AuthDbContext>();
 
+builder.Services.Configure<IdentityOptions>(options =>
+{
+    options.Password.RequireDigit = true;
+    options.Password.RequireLowercase = true;
+    options.Password.RequireNonAlphanumeric = true;
+    options.Password.RequireUppercase = true;
+    options.Password.RequiredLength = 6;
+    options.Password.RequiredUniqueChars = 1;
+});
+
 
 builder.Services.AddScoped<ITagRepository, TagRepository>();
 builder.Services.AddScoped<IBlogPostRepository, BlogPostRepository>();
