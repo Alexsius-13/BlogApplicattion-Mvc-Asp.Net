25e9f06318f6537cb94881f3ea364cf5f394d770
Aleko Kotashvili <alexkotashvili@gmail.com>
2024-06-27 15:09:51 +0400

    Implemented additional functionality of admin that refers to displaying all users on screen. Only signed-in admins or single super-admin can enjoy this right with the help of authorization

---
+++ Bloggie/Bloggie.Web/Controllers/AdminUsersController.cs
@@ -0,0 +1,36 @@
+﻿using Bloggie.Web.Models.ViewModels;
+using Bloggie.Web.Repositories;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Bloggie.Web.Controllers
+{
+	[Authorize(Roles = "Admin")]
+	public class AdminUsersController : Controller
+	{
+		private readonly IUserRepository userRepository;
+
+		public AdminUsersController(IUserRepository userRepository)
+        {
+			this.userRepository = userRepository;
+		}
+        public async Task<IActionResult> List()
+		{
+			var users = await userRepository.GetAll();
+			var userViewModel = new UserViewModel();
+			userViewModel.Users = new List<User>();
+
+			foreach (var user in users)
+			{
+				userViewModel.Users.Add(new Models.ViewModels.User
+				{
+					Id = Guid.Parse(user.Id),
+					Username = user.UserName,
+					Email = user.Email,
+				});
+			}
+
+			return View(userViewModel);
+		}
+	}
+}
