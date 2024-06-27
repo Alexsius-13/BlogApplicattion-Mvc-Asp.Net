80ed853761621740a9317763cbcfc8fd7a20bcb8
Aleko Kotashvili <alexkotashvili@gmail.com>
2024-06-27 16:23:25 +0400

    Added unique superAdmin functionality of creating default users as well as admin users. Admin is restricted of creating admin user but only has choice of adding default one.

--- Bloggie/Bloggie.Web/Controllers/AdminUsersController.cs
+++ Bloggie/Bloggie.Web/Controllers/AdminUsersController.cs
@@ -1,6 +1,7 @@
 ﻿using Bloggie.Web.Models.ViewModels;
 using Bloggie.Web.Repositories;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Bloggie.Web.Controllers
@@ -9,11 +10,16 @@ namespace Bloggie.Web.Controllers
 	public class AdminUsersController : Controller
 	{
 		private readonly IUserRepository userRepository;
+        private readonly UserManager<IdentityUser> userManager;
 
-		public AdminUsersController(IUserRepository userRepository)
+        public AdminUsersController(IUserRepository userRepository, 
+			UserManager<IdentityUser> userManager)
         {
 			this.userRepository = userRepository;
-		}
+            this.userManager = userManager;
+        }
+
+		[HttpGet]
         public async Task<IActionResult> List()
 		{
 			var users = await userRepository.GetAll();
@@ -31,6 +37,39 @@ namespace Bloggie.Web.Controllers
 			}
 
 			return View(userViewModel);
+		}
+
+		[HttpPost]
+		public async Task<IActionResult> List(UserViewModel request)
+		{
+			var identityUser = new IdentityUser
+			{
+				UserName = request.Username,
+				Email = request.Email
+			};
+
+			var identityResult = await userManager.CreateAsync(identityUser, request.Password);
+			if(identityResult is not null)
+			{
+				if (identityResult.Succeeded)
+				{
+					var roles = new List<string> { "User" };
+
+					if(request.AdminRoleCheckBox)
+					{
+						roles.Add("Admin");
+					}
+
+					identityResult = await userManager.AddToRolesAsync(identityUser, roles);
+
+					if(identityResult is not null && identityResult.Succeeded)
+					{
+						return RedirectToAction("List", "AdminUsers");
+					}
+				}
+			}
+
+			return View();
 		}
 	}
 }
--- Bloggie/Bloggie.Web/Models/ViewModels/UserViewModel.cs
+++ Bloggie/Bloggie.Web/Models/ViewModels/UserViewModel.cs
@@ -3,5 +3,9 @@
 	public class UserViewModel
 	{
         public List<User> Users { get; set; }
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public bool AdminRoleCheckBox { get; set; }
     }
 }
